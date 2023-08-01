1b2ef4dc701defddd8bfc307fdc635beac82cdde
ZukAd14 <123511514+ZukAd14@users.noreply.github.com>
2023-08-01 18:29:37 +0000

    F*****G_Replit

--- server.js
+++ server.js
@@ -14,47 +14,50 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use((req, res, next) => {
-    req.io = io;
-    next();
-});
+
 app.use(express.static(path.join(__dirname, '/client/build')));
-app.use('/api', testimonialsRoutes);
-app.use('/api', concertsRoutes);
-app.use('/api', seatsRoutes);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
-
-app.use((req, res) => {
-    res.status(404).json({ message: 'Not found...' });
-})
 
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if(NODE_ENV === 'production') dbUri = 'url to remote db';
-else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/newWaveDBtest';
-else dbUri = 'mongodb+srv://AdminxD:kurkawodna@cluster0.sdxou7a.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
+if (NODE_ENV === 'production') dbUri = 'mongodb+srv://AdminxD:${process.env.DB_PASS}@cluster0.sdxou7a.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
+else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/newWaveDBtest';
+else dbUri = 'mongodb://localhost:27017/NewWaveDB';
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
-    console.log('Connected to the database');
+  console.log('Connected to the database');
 });
 db.on('error', err => console.log('Error ', + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port: 8000');
+  console.log('Server is running on port: 8000');
 });
 
-const io = socket(server);
+const io = socket(server)
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
+});
 
 io.on('connection', (socket) => {
-    console.log('New socket!');
-    console.log('New client! Its id – ' + socket.id);
+  console.log('New socket!');
+  console.log('New client! Its id – ' + socket.id);
 });
 
 module.exports = server;
